c91f5e4aa9995b12970285c87f66b22e7c34470e
Minoesil <75186131+Minoesil@users.noreply.github.com>
2020-11-29 20:59:28 -0300

    movimiento de cámara y sprite

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -20,6 +20,36 @@ namespace myTiles {
     export const tile8 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile13 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile14 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile15 = image.ofBuffer(hex``);
+
+    helpers.registerTilemapFactory(function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level": return tiles.createTilemap(hex`0a000a0004030303030303030308050707070707070707060507070707070707070601020101090101020101010201010901010201010102010109010102010101020101090101020101010201010901010201010102010109010102010101020101090101020101`, img`
+2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8,myTiles.tile9], TileScale.Sixteen)
+        }
+        return null;
+    })
 
 }
 // Auto-generated code. Do not edit.
